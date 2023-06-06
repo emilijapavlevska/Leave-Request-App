f3885c9b472085726c12972e10bdcf96c9be1504
emilijapavlevska <73494422+emilijapavlevska@users.noreply.github.com>
2023-06-06 14:03:08 +0200

    firebase authentication login

--- app/src/main/java/com/example/leaverequestapp/MainActivity.java
+++ app/src/main/java/com/example/leaverequestapp/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.leaverequestapp;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FirebaseApp firebaseApp = FirebaseApp.initializeApp(this);
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance(firebaseApp);
+
         signInButton = findViewById(R.id.signInButton);
         newRequestButton = findViewById(R.id.newRequestButton);
         historyButton = findViewById(R.id.historyButton);
@@ -30,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
 
         if (isSignedIn) {
             signInButton.setText("Sign Out");
+            signin();
+
             newRequestButton.setVisibility(View.VISIBLE);
             historyButton.setVisibility(View.VISIBLE);
         } else {
@@ -42,6 +56,25 @@ public class MainActivity extends AppCompatActivity {
     public void onNewRequestButtonClicked(View view) {
         Intent intent = new Intent(this, NewRequestActivity.class);
         startActivity(intent);
+    }
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            (result) -> {
+                Toast.makeText(getApplicationContext(), "Logged in", Toast.LENGTH_SHORT).show();
+            });
+
+    public void signin() {
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Arrays.asList(
+                        new AuthUI.IdpConfig.GoogleBuilder().build(),
+                        new AuthUI.IdpConfig.FacebookBuilder().build(),
+                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.AnonymousBuilder().build()))
+                .build();
+
+        signInLauncher.launch(signInIntent);
     }
 
     public void onHistoryButtonClicked(View view) {
